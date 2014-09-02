e3b2e3bf249a62cced3bd5ef5b7278d79d90e8f2
Rainer Su <rainersu@foxmail.com>
2014-09-02 22:56:10 +0800

    Initial commit

--- jekyll_src/js/docs/grid.js
+++ jekyll_src/js/docs/grid.js
@@ -1,1 +1,14 @@
-window.alert("shit");
+!function(a) {
+    a(function() {
+        var b = a("<span />").prependTo(a(".col").each(function() {
+            var b = a(this);
+            b.attr("title", b.prop("className"));
+        })), c = function() {
+            b.each(function() {
+                var b = a(this), c = b.parent();
+                b.text(c.css("width"));
+            });
+        };
+        a(window).resize(c).resize();
+    });
+}(jQuery);
--- js/docs/grid.js
+++ js/docs/grid.js
@@ -1,1 +1,25 @@
-window.alert("shit");
+
+(function($, _) {
+
+    $(function() {
+        
+        var
+            aColSpan = $('<span />').prependTo($('.col').each(function() {
+                var 
+                    o = $(this);
+                o.attr('title', o.prop('className'));
+            })),
+            fColSpan = function() {
+                aColSpan.each(function() {
+                    var 
+                        o = $(this),
+                        p = o.parent();
+                    o.text(p.css('width'));
+                });
+            };
+        $(window).resize(fColSpan).resize();
+        
+    });
+    
+}(jQuery));
+ 
